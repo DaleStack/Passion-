491c631cab4756d5b8df4994f348367bf9539a23
Dale-devv <Thisiskei07@gmail.com>
2025-05-14 20:08:27 +0800

    updated automatic slug

--- seller/models.py
+++ seller/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
-from django.utils.crypto import get_random_string
-# Create your models here.
 
 class CategoryModel(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
+            # Ensure uniqueness if needed
+            counter = 1
+            original_slug = self.slug
+            while CategoryModel.objects.filter(slug=self.slug).exists():
+                self.slug = f"{original_slug}-{counter}"
+                counter += 1
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.name
+
 
 class ProductModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='products')
     category = models.ForeignKey(CategoryModel, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=100, null=False)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         unique_together = ('user', 'slug')
@@ -42,9 +46,5 @@ class ProductModel(models.Model):
             self.slug = slug
         super().save(*args, **kwargs)
 
-        
     def __str__(self):
         return self.name
-
-    
-
