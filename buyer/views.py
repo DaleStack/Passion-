d4e1e2c216ae9616c850411a14b6cd9fa9243c1c
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-05-17 10:25:01 +0800

    added buyer app

---
+++ buyer/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from seller.models import ProductModel
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+# Create your views here.
+@login_required
+def buyer_dashboard(request):
+    user = request.user
+    
+    if user.role != 'Buyer':
+        messages.error(request, 'You are not authorized to view this page')
+        return redirect('seller:seller_dashboard')
+    
+    products = ProductModel.objects.all()
+
+    context = {
+        'user': user,
+        'products': products
+    }
+    return render(request, 'buyer/dashboard.html', context)
