60bcca3b1943ba169081ab1359143a2ef16f4d75
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-05-18 16:44:14 +0800

    buying function done

--- buyer/urls.py
+++ buyer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import buyer_dashboard
+from .views import buyer_dashboard, item_view
 
 app_name = 'buyer'
 
 urlpatterns = [
     path('buyer-dashboard/', buyer_dashboard, name="buyer_dashboard"),
+    path('item/<int:pk>/', item_view, name="item_view"),
 ]
--- buyer/views.py
+++ buyer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from seller.models import ProductModel
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import OrderForm
 # Create your views here.
 @login_required
 def buyer_dashboard(request):
@@ -23,9 +24,31 @@ def buyer_dashboard(request):
 def item_view(request, pk):
     product = get_object_or_404(ProductModel, pk=pk)
 
-    return render(request, 'buyer/product_detail.html', {
-        'product': product
-    })
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if order.quantity > product.stock:
+                messages.error(request, f"Only {product.stock} items in stock.")
+                return redirect('buyer:item_view', pk=pk)
 
+            order.product = product
+            order.buyer = request.user
+            order.is_paid = True  # Mark as paid
+            order.save()
+
+            # Reduce product stock
+            product.stock -= order.quantity
+            product.save()
+
+            messages.success(request, "Item added to your orders!")
+            return redirect('buyer:buyer_dashboard')
+    else:
+        form = OrderForm()
+
+    return render(request, 'buyer/item_view.html', {
+        'product': product,
+        'form': form,
+    })
 
 
